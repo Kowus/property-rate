cde33ccb946809c226bfbc688ae0f71ac86675b5
Kowus <barnabas@elite-education.org>
2018-02-22 16:38:36 +0000

    art

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(passport.session());
 app.use(flash());
 app.use((req, res, next) => {
     res.locals.user = req.user || null;
-    if (req.user &&req.user.group&& req.user.group == 'user') res.locals.acc_zone=true;
+    if (req.user &&req.user.group&& req.user.group == 'user') res.locals.acc_zonee=true;
     next();
 });
 
