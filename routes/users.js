5c8af95875f5bb7cd7ebf67c15993803ba843f20
Kowus <barnabas@elite-education.org>
2018-02-27 17:16:25 +0000

    update users

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ var express = require('express'),
     async = require('async'),
     mail = require('../config/sendmail'),
     Bill = require('../lib/bill')
-;
+    ;
 router.get('/', function (req, res, next) {
     res.render('s-create-user');
 });
@@ -16,12 +16,26 @@ router.get('/', function (req, res, next) {
 router.get('/create', function (req, res, next) {
     res.render('create-user');
 });
-router.post('/create', function (req, res, next) {
+router.post('/update', needsGroup('admin'), function (req, res, next) {
+    User.findOneAndUpdate({ _id: req.body._id }, {
+        $set: {
+            givenName: req.body.givenName,
+            familyName: req.body.familyName,
+            email: req.body.email,
+            displayName: `${req.body.givenName} ${req.body.familyName}`
+        }
+    }, function (err) {
+        if (err) console.error(err);
+        res.redirect('/users/' + req.body._id);
+    });
+
+});
+router.post('/create', needsGroup('admin'), function (req, res, next) {
     // res.send(req.body)
-    User.findOne({email: req.body.email}, function (err, user) {
+    User.findOne({ email: req.body.email }, function (err, user) {
         if (err) return done(err);
         if (user) {
-            res.render('create-user', {message: 'That email has already been used with an account.'});
+            res.render('create-user', { message: 'That email has already been used with an account.' });
         } else {
             let gib = require('../lib/gibberish');
             let pwd = gib();
@@ -37,11 +51,11 @@ router.post('/create', function (req, res, next) {
                 if (err) {
                     console.log(err);
                     // return done(null, false, req.flash('signupMessage', "Couldn't create your account."));
-                    return res.render('create-user', {message: "Couldn't create the account."});
+                    return res.render('create-user', { message: "Couldn't create the account." });
                 }
                 // return done(null,user);
                 mail.newUser(user, pwd);
-                res.render('create-user', {success_message: `Successfully created user with _id: ${user._id} and email: ${user.email}.`});
+                res.render('create-user', { success_message: `Successfully created user with _id: ${user._id} and email: ${user.email}.` });
 
             });
         }
@@ -49,10 +63,10 @@ router.post('/create', function (req, res, next) {
 });
 
 
-router.post('/add_prop', function (req, res, next) {
+router.post('/add_prop', needsGroup('admin'), function (req, res, next) {
     let rcn = req.body.use_rate < 10 ? Math.random() * 9 : 0,
         dep = req.body.use_rate < 10 ? Math.random() * 6 : 0
-    ;
+        ;
     let newProp = new Property({
         prop_num: req.body.prop_num,
         len: req.body.len,
@@ -76,7 +90,7 @@ router.post('/add_prop', function (req, res, next) {
         async.parallel(
             [
                 function (callback) {
-                    User.findOneAndUpdate({_id: prop.owner}, {
+                    User.findOneAndUpdate({ _id: prop.owner }, {
                         $push: {
                             properties: {
                                 // how to do push on Update
@@ -89,19 +103,19 @@ router.post('/add_prop', function (req, res, next) {
                         callback();
                     });
                 }, function (callback) {
-                Area.findOneAndUpdate({_id: prop.area}, {
-                    $push: {
-                        properties: {
-                            // how to do push on Update
-                            $each: [prop._id],
-                            $position: 0
+                    Area.findOneAndUpdate({ _id: prop.area }, {
+                        $push: {
+                            properties: {
+                                // how to do push on Update
+                                $each: [prop._id],
+                                $position: 0
+                            }
                         }
-                    }
-                }).exec(function (err) {
-                    if (err) return callback(err);
-                    callback();
-                });
-            }],
+                    }).exec(function (err) {
+                        if (err) return callback(err);
+                        callback();
+                    });
+                }],
             (err, result) => {
                 if (err) return res.send(err);
                 console.log(prop);
@@ -117,7 +131,7 @@ router.post('/add_prop', function (req, res, next) {
 
 router.get('/:user_id', function (req, res, next) {
     // console.log(req.params)
-    User.findOne({_id: req.params.user_id}).populate({
+    User.findOne({ _id: req.params.user_id }).populate({
         path: 'properties bill transactions',
         populate: [
             {
@@ -145,10 +159,22 @@ router.get('/:user_id', function (req, res, next) {
         if (err) console.error(err);
         let transid = req.session.transid || null,
             transtat = req.session.transtat || null
-        ;
-        req.session.transid=undefined;
-        req.session.transtat=undefined;
-        res.render('user', {owner: user, use_code: use_code, sanitation: sanitation, transid:transid, transtat});
+            ;
+        req.session.transid = undefined;
+        req.session.transtat = undefined;
+        res.render('user', { owner: user, use_code: use_code, sanitation: sanitation, transid: transid, transtat });
     });
 });
 module.exports = router;
+function needsGroup(group) {
+    return function (req, res, next) {
+        if (req.user && req.user.group === group) {
+            res.locals.user = req.user;
+            next();
+        }
+        else {
+            req.session.message = "Unauthorized Access";
+            res.status(401).redirect(`/login?next=${req.originalUrl}`);
+        }
+    };
+}
