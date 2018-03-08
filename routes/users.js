498e5c28d857ce367c8f6afb811459256b93314b
Kowus <barnabas@elite-education.org>
2018-03-08 18:32:51 +0000

    isuser _id

--- routes/users.js
+++ routes/users.js
@@ -162,7 +162,7 @@ router.get('/:user_id', function (req, res, next) {
             ;
         req.session.transid = undefined;
         req.session.transtat = undefined;
-        res.render('user', { owner: user, use_code: use_code, sanitation: sanitation, transid: transid, transtat });
+        res.render('user', { isuser: req.user._id == user._id, owner: user, use_code: use_code, sanitation: sanitation, transid: transid, transtat });
     });
 });
 module.exports = router;
